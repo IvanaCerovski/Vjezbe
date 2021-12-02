0f98c2c1c757b7acd5dc38f04e4bf575127cff34
IvanaCerovski <93525137+IvanaCerovski@users.noreply.github.com>
2021-12-02 11:23:12 +0100

    Dodana tablica customers
    Atributi i primarni ključ

--- coffeeshop.sql
+++ coffeeshop.sql
@@ -9,3 +9,12 @@ price decimal(3,2)
 );
 
 show tables;
+
+create table customers(
+id int primary key auto_increment,
+first_name varchar(30),
+last_name varchar(30),
+gender enum('M','F'),
+phone_number varchar(20)
+);
+
