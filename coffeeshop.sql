954f443cf88827ffecb2341687cf74a01b128ff6
IvanaCerovski <93525137+IvanaCerovski@users.noreply.github.com>
2021-12-02 11:10:58 +0100

    Dodana tablica products
    Dodana tablica i atributi, primarni ključ

--- coffeeshop.sql
+++ coffeeshop.sql
@@ -1,3 +1,9 @@
 drop database if exists coffeeshop;
 create database coffeeshop;
 use coffeeshop;
+
+create table products(
+id int primary key auto_increment,
+name varchar(30),
+price decimal(3,2)
+);
