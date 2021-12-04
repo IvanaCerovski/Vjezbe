0637b8f29ad04cb0540f6ffb9bbb0c5a41b34267
IvanaCerovski <93525137+IvanaCerovski@users.noreply.github.com>
2021-12-04 20:02:45 +0100

    Delete jedne osobe iz tablice

--- example.sql
+++ example.sql
@@ -48,3 +48,6 @@ insert into people (name,age,gender)
 values ('Emma',21,'F'),('John',30,'M'),('Thomas',27,'M'),('Chris',44,'M'),('Sally',23,'F'),('Frank',55,'M');
 delete from people 
 where id > 7;
+
+delete from people 
+where name='John';
