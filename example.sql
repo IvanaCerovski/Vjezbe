3d2ac7283e447626b3f98e5383a25dbc8a818a4c
IvanaCerovski <93525137+IvanaCerovski@users.noreply.github.com>
2021-12-04 20:01:22 +0100

    Greškom dodani 2x insert - obrisani dupli

--- example.sql
+++ example.sql
@@ -41,3 +41,10 @@ name varchar(30),
 age int,
 gender enum('M','F')
 );
+
+select * from people;
+
+insert into people (name,age,gender)
+values ('Emma',21,'F'),('John',30,'M'),('Thomas',27,'M'),('Chris',44,'M'),('Sally',23,'F'),('Frank',55,'M');
+delete from people 
+where id > 7;
