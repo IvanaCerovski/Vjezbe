744f083dd0480c981ea78832dc01812213358a45
IvanaCerovski <93525137+IvanaCerovski@users.noreply.github.com>
2021-12-06 00:37:22 +0100

    Select - between

--- coffeeshop.sql
+++ coffeeshop.sql
@@ -220,3 +220,6 @@ select * from orders;
 select product_id,customer_id,order_time from orders 
 where order_time between '2017-01-01' and '2017-01-07';
 
+select product_id,customer_id,order_time from orders 
+where customer_id between 5 and 10;
+
